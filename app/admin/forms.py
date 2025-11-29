b8dda04d7a087bd0f3973522a0300cd26cd26264
NEW_USERNAME <NEW_EMAIL>
2025-11-29 14:27:59 +0500

    Fix: Add missing FlaskForm import

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -1,3 +1,4 @@
+from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, FloatField, IntegerField, FileField, SelectField
 from wtforms.validators import DataRequired
 
